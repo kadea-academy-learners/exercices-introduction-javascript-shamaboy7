f304b3ecb667a48ef297a9a1099da2a51336e3d9
shamboy7 <shamadaniel30@gmail.com>
2025-10-15 20:16:51 +0100

    feat: Inheritance allocation of Mister John Mukuna

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,2 +1,23 @@
-// Exercice 9
-// Écrivez votre code ici
+let inheritance = 60000000 + 40000000 + 20000000; // totale iheritance
+console.log("The overall inreritance will be: " + inheritance + "CDF")
+let firstCategory = inheritance * 0.75; // 75 %
+console.log("The first category inheritance will get: " + firstCategory + "CDF")
+let secondCategory = inheritance * 0.25; // 25 %
+console.log("The second category inheritance will get: " + secondCategory + "CDF")
+let partOne = firstCategory / 3; // parts for firstCategory
+let paul = partOne;
+let marie = partOne;
+let alain = partOne
+let eric = alain / 2;
+let claire = alain / 2;
+console.log("Paul aura " + paul + "CDF");
+console.log("marie aura " + marie + "CDF");
+console.log("eric aura " + eric + "CDF");
+console.log("claire aura " + claire + "CDF")
+let partTwo = secondCategory / 3;
+let madameMukuna = partTwo;
+let joseph = partTwo;
+let sarah = partTwo;
+console.log("Madame Mukuna aura " + partTwo + "CDF");
+console.log("Joseph aura " + partTwo + "CDF");
+console.log("Sarah aura " + partTwo + " CDF");
