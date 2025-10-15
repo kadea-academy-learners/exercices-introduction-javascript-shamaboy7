4034373494cb9c379fa05289884831fa5cb2ed36
shamboy7 <shamadaniel30@gmail.com>
2025-10-15 17:01:36 +0100

    feat: Writing a little script to convert Celcius temperature into Fahrenheit and display the result in the console

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,7 @@
-// Exercice 5
-// Écrivez votre code ici
+function convertToFahrenheit(celsius){
+  return (celsius * 9/5) +32;
+}
+let celsius = 13;
+let fahrenheit = Math.round(convertToFahrenheit(celsius));
+
+console.log(`${celsius}°C est égal à ${fahrenheit}°F`);
