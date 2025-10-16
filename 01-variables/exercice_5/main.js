e2ba1ade49763071460712368e1d699376b35bb8
shamboy7 <shamadaniel30@gmail.com>
2025-10-16 18:49:43 +0100

    Fix: Using module.exports

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,7 +1,16 @@
-function convertToFahrenheit(celsius){
+/*function convertToFahrenheit(celsius){
   return (celsius * 9/5) +32;
 }
-let celsius = 13;
-let fahrenheit = Math.round(convertToFahrenheit(celsius));
+let celsius = 25;
+let fahrenheit = convertToFahrenheit(celsius);
 
 console.log(`${celsius}°C est égal à ${fahrenheit}°F`);
+module.exports = {convertToFahrenheit};
+*/
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5)  + 32;
+}
+let celsius =25;
+//let fahrenheit = convertToFahrenheit//nm(celsius);
+console.log(celsiusToFahrenheit(celsius));
+module.exports = { celsiusToFahrenheit }
